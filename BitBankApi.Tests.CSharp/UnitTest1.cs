46c9a26233bd34893b61c984698e59da3b8087e7
joemphilips <joemphilips@gmail.com>
2019-02-18 09:20:10 +0900

    Use task {} expression instead of async everywhere

--- BitBankApi.Tests.CSharp/UnitTest1.cs
+++ BitBankApi.Tests.CSharp/UnitTest1.cs
@@ -7,51 +7,53 @@ using System.Threading.Tasks;
 
 namespace BitBankApi.Tests.CSharp
 {
-  public class BitBankApiTest1
-  {
-    [Fact]
-    public void PublicApiTest1()
+    public class BitBankApiTest1
     {
-      var api = new PublicApi();
-      var ticker = api.GetTicker("btc_jpy");
-      Console.WriteLine(ticker.ToString());
-      Assert.NotNull(ticker);
+        [Fact]
+        public void PublicApiTest1()
+        {
+            var api = new PublicApi();
+            var ticker = api.GetTicker("btc_jpy");
+            Console.WriteLine(ticker.ToString());
+            Assert.NotNull(ticker);
+        }
+
+        [Fact]
+        public void PrivateApiTest1()
+        {
+            using (var api = GetPrivate())
+            {
+                var resp = api.GetAssets();
+                Assert.True(resp.Success == 1);
+                Assert.NotNull(resp.Data.Assets);
+            }
+        }
+
+        [Fact]
+        public async void PrivateAsyncApiTest1()
+        {
+            using (var api = GetPrivate())
+            {
+                var resp = await api.GetAssetsAsync();
+                Assert.True(resp.Success == 1);
+                Assert.NotNull(resp.Data.Assets);
+
+                var resp2 = await api.GetTradeHistoryAsync();
+                Assert.True(resp2.Success == 1);
+                Assert.NotNull(resp2.Data);
+
+            }
+        }
+
+        private static BitBankApi.PrivateApi GetPrivate()
+        {
+
+            var conf = JObject.Parse(File.ReadAllText("../../../../BitBankApi/config.json"));
+            var apiKey = (string)(conf.GetValue("ApiKey"));
+
+            var apiSecret = (string)conf.GetValue("ApiSecret");
+
+            return new PrivateApi(apiKey, apiSecret);
+        }
     }
-
-    [Fact]
-    public void PrivateApiTest1()
-    {
-      using (var api = GetPrivate())
-      {
-        var resp = api.GetAssets();
-        Assert.True(resp.Success == 1);
-        Assert.NotNull(resp.Data.Assets);
-      }
-    }
-
-    [Fact] async void PrivateAsyncApiTest1()
-    {
-      using (var api = GetPrivate())
-      {
-        var resp = await api.GetAssetsAsync();
-        Assert.True(resp.Success == 1);
-        Assert.NotNull(resp.Data.Assets);
-
-        var resp2 = await api.GetTradeHistoryAsync();
-        Assert.True(resp2.Success == 1);
-        Assert.NotNull(resp2.Data);
-      }
-    }
-
-    private static BitBankApi.PrivateApi GetPrivate()
-    {
-
-      var conf = JObject.Parse(File.ReadAllText("../../../../BitBankApi/config.json"));
-      var apiKey = (string)(conf.GetValue("ApiKey"));
-
-      var apiSecret = (string)conf.GetValue("ApiSecret");
-
-      return new PrivateApi(apiKey, apiSecret);
-    }
-  }
 }
